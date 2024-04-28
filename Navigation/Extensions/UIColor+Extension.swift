de035a95c716926d8f9fdf0051c4eb953c1ecc5a
Name <usmanovruslan2801@icloud.com>
2024-04-28 15:47:32 +0300

    Home Work 3+

--- Navigation/Extensions/UIColor+Extension.swift
+++ Navigation/Extensions/UIColor+Extension.swift
@@ -9,25 +9,21 @@ import Foundation
 import UIKit
 
 extension UIColor {
-    static let  customViewBackGroundColor = UIColor(dynamicProvider: { traitCollection in
-        let darkColor = UIColor.systemGray
-        let otherColor = UIColor.systemGray6
-        return(traitCollection.userInterfaceStyle == .dark ?  darkColor :  otherColor)
-    })
-    static let  customControllerBackGroundColor = UIColor(dynamicProvider: { traitCollection in
-        let darkColor = UIColor.black
-        let otherColor = UIColor.white
-        return(traitCollection.userInterfaceStyle == .dark ?  darkColor :  otherColor)
-    })
-    static let  customTintColor = UIColor(dynamicProvider: { traitCollection in
-        let darkColor = UIColor.white
-        let otherColor = UIColor.black
-        return(traitCollection.userInterfaceStyle == .dark ?  darkColor :  otherColor)
-    })
-    static let  customImageBackGrounColor = UIColor(dynamicProvider: { traitCollection in
-        let darkColor = UIColor.black
-        let otherColor = UIColor.white
-        return(traitCollection.userInterfaceStyle == .dark ?  darkColor :  otherColor)
-    })
+    
+    static let  customViewBackGroundColor = createColor(lightMode: UIColor.systemGray6, darkMode: UIColor.systemGray)
+    static let  customControllerBackGroundColor = createColor(lightMode: UIColor.white, darkMode: UIColor.black)
+    static let  customTintColor = createColor(lightMode: UIColor.black, darkMode: UIColor.white)
+    static let  customImageBackGrounColor = createColor(lightMode: UIColor.white, darkMode: UIColor.black)
+    
+    static func createColor(lightMode: UIColor, darkMode: UIColor) -> UIColor {
+        guard #available(iOS 13.0, *) else {
+            return lightMode
+        }
+        return UIColor { (traitCollection) -> UIColor in
+            return traitCollection.userInterfaceStyle == .light ? lightMode :
+            darkMode
+        }
+    }
 }
+
                                     
